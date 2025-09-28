a3549a2594b2acb4f0576b3ca2736fc1aadf527a
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2025-09-28 16:48:34 +0200

    added stuff to main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 
 def main():
     node = TextNode("dummy text", TextType.URL, url="http://www.example.com")
+    node1 = HTMLNode(
+        "a",
+        "http://google.com",
+        children=None,
+        props={"href": "http://google.com", "target": "_blank"},
+    )
+    print(node1.props_to_html())
     print(node)
 
 
