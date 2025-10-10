4db7d1e0be2a3efe8a412756c076c9b85d427bd5
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2025-10-10 23:39:37 +0200

    fixed type

--- src/generate_page.py
+++ src/generate_page.py
@@ -55,7 +55,7 @@ def generate_page(from_path, template_path, dest_path, basepath):
         index = (
             template.replace("{{ Title }}", title)
             .replace("{{ Content }}", html)
-            .replace("href=/", f"href=/{basepath}")
+            .replace("href=/", f"href={basepath}")
             .replace("src=/", f"src={basepath}")
         )
 
